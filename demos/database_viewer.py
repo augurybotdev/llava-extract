463ecc88e5124e44ed52f23c5c95190013698247
augurybot3 <augurybot@gmail.com>
2023-11-03 03:13:14 -0400

    fix image upload on demo

--- demos/database_viewer.py
+++ demos/database_viewer.py
@@ -1,6 +1,8 @@
 # !pip install SQLAlchemy mysqlclient
+import os
+import tempfile
 import streamlit as st
-from custom_modules import Extract
+from ext import Extract
 import sqlite3
 import pandas as pd
 
@@ -20,13 +22,28 @@ def run_main():
         df = get_data(table_name)
         st.write(df)
         
+    
+        
     form = st.form("a form")
     with form:
         upload = st.file_uploader("upload images to extract text", type=[".png", ".jpg", ".jpeg"])
+
+        if upload is not None:
+            temp_dir = tempfile.mkdtemp()
+            file_name = upload.name
+            temp_file_path = os.path.join(temp_dir, file_name)
+            
+            with open(temp_file_path, "wb") as temp_file:
+                temp_file.write(upload.read())
+
         extract = Extract()
         extract_button = st.form_submit_button("Extract Text From Image")
         if extract_button:
-            extract.process_input(upload)
+            extract.process_input(temp_file_path)
+            
+            os.remove(temp_file_path)
+            os.rmdir(temp_dir)
+
 
 if __name__ == '__main__':
     run_main()
